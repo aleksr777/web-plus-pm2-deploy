77e9e1d903ac2be53ea858b48156fa7323be9dde
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-06-11 14:05:56 +0500

    Добавляет деплой бекенда

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
+  process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto",
+      script: "dist/app.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
